c1d5a204654451ef5b47927c940d87030a983cb9
Екатерина Кондрашкина <kkondrashkina@gmail.com>
2019-10-21 23:09:05 +0700

    Добавлена задача HashTable.

---
+++ MyHashTable/HashTable.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyHashTable
+{
+    class HashTable<T> : ICollection<T>
+    {
+        private List<T>[] items;
+
+        public HashTable(int size)
+        {
+            items = new List<T>[size];
+        }
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void Add(T item)
+        {
+            int index = Math.Abs(item.GetHashCode() % items.Length);
+
+            if (items[index] == null)
+            {
+                items[index] = new List<T>();
+            }
+
+            items[index].Add(item);
+
+            Count++;
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                items[i] = null;
+            }
+
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] == null)
+                {
+                    continue;
+                }
+
+                foreach (T tableItem in items[i])
+                {
+                    if (tableItem.Equals(item))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array.Length - arrayIndex < Count)
+            {
+                throw new ArgumentException("Размер массива слишком мал.", nameof(array));
+            }
+
+            int index = 0;
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] == null)
+                {
+                    continue;
+                }
+
+                for (int j = 0; j < items[i].Count; j++)
+                {
+                    array[arrayIndex + index] = items[i][j];
+
+                    index++;
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] == null)
+                {
+                    continue;
+                }
+
+                foreach (T tableItem in items[i])
+                {
+                    yield return tableItem;
+                }
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] == null)
+                {
+                    continue;
+                }
+
+                if (items[i].Remove(item))
+                {
+                    if (items[i].Count == 0)
+                    {
+                        items[i] = null;
+                    }
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                sb.Append("[ ");
+
+                if (items[i] == null)
+                {
+                    sb.Append(']')
+                        .Append(Environment.NewLine);
+
+                    continue;
+                }
+
+                foreach (T tableItem in items[i])
+                {
+                    sb.Append(tableItem)
+                        .Append(", ");
+                }
+
+                sb.Remove(sb.Length - 2, 1)
+                    .Append(']')
+                        .Append(Environment.NewLine);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ MyHashTable/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MyHashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable<string> table = new HashTable<string>(40);
+
+            for (int i = 0; i < 30; i++)
+            {
+                table.Add($"{i}");
+            }
+
+            Console.WriteLine(table);
+            Console.WriteLine("Количество элементов в таблице = {0}", table.Count);
+            Console.WriteLine();
+
+            string number = "15";
+
+            if (table.Contains(number))
+            {
+                Console.WriteLine("Таблица содержит число {0}", number);
+            }
+            else
+            {
+                Console.WriteLine("Таблица не содержит число {0}", number);
+            }
+
+            Console.WriteLine();
+
+            table.Remove("17");
+            table.Remove("5");
+
+            Console.WriteLine("Теперь таблица равна:");
+            Console.WriteLine(table);
+            Console.WriteLine();
+
+            string[] newTable = new string[30];
+
+            table.CopyTo(newTable, 0);
+
+            Console.WriteLine("Список с копией масива: ");
+            Console.WriteLine(string.Join(", ", newTable));
+            Console.WriteLine();
+
+            table.Clear();
+
+            Console.WriteLine("Теперь таблица равна:");
+            Console.WriteLine(table);
+            Console.WriteLine();
+        }
+    }
+}
